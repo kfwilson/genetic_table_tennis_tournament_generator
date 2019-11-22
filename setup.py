7fcf43c19c340b6d580992d570142740a32ff2a0
Kate Wilson <kawils@microsoft.com>
2019-11-22 10:26:21 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='genetic_tournament_generator',
+    version='0.1',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    include_package_data=True,
+    install_requires=[
+    ],
+)
